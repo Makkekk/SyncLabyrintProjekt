b4c7f48fd3fa6414fd0fdadd0add2bd454aa2581
Karsten <karsten-kirkegaard@hotmail.com>
2025-09-21 19:10:02 +0200

    Broadcast

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,12 +1,40 @@
+import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TCPServer {
+    private static List<Socket> clients = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
         ServerSocket welcomeSocket = new ServerSocket(9000);
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
-            (new SendThread(connectionSocket)).start();
-//            (new RecieveThread(connectionSocket)).start();
+            clients.add(connectionSocket);
+            new Thread(() -> håndterClient(connectionSocket).start();
+        }
+    }
+    private static void håndterClient(Socket socket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String message;
+            while ((message = in.readLine()) != null) {
+                System.out.println("Server modtog: " + message);
+                broadcast(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void broadcast(String message) {
+        for (Socket client : clients) {
+            try {
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                out.writeBytes(message + "\n");
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
