ec224e17236c8fdc89667b028fce08d9beb0c8f7
KarstenK <karsten-kirkegaard@hotmail.com>
2025-09-22 21:01:50 +0200

    update til branch

--- src/RecieveThread.java
+++ src/RecieveThread.java
@@ -1,45 +1,38 @@
 import javafx.application.Platform;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.Socket;
-import java.net.SocketException;
-import java.util.Objects;
 
-public class RecieveThread extends Thread{
-    Socket socket;
-    String message;
+public class RecieveThread extends Thread {
+    private Socket socket;
+    private GUI gui;
 
-    public RecieveThread(Socket socket) throws Exception {
+    public RecieveThread(Socket socket, GUI gui) {
         this.socket = socket;
+        this.gui = gui;
     }
 
+    @Override
     public void run() {
-        try {
-            BufferedReader inFromAfsender = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println("Forbundet til server.");
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String message;
+            while ((message = in.readLine()) != null) {
+                System.out.println("From server: " + message);
 
-            while (true) {
-                message = inFromAfsender.readLine();
-                System.out.println("From sender: " + message);
-                String[] messageArray = message.split(" ");
-                if (message!= null){
-                    if (Objects.equals(messageArray[1], "up")) {
-                        Platform.runLater(() -> GUI.playerMoved(messageArray[0], 0, -1, "up"));
-                    } else if (Objects.equals(messageArray[1], "down")) {
-                        Platform.runLater(() -> GUI.playerMoved(messageArray[0], 0, +1, "down"));
-                    } else if (Objects.equals(messageArray[1], "left")) {
-                        Platform.runLater(() -> GUI.playerMoved(messageArray[0], -1, 0, "left"));
-                    } else if (Objects.equals(messageArray[1], "right")) {
-                        Platform.runLater(() -> GUI.playerMoved(messageArray[0], +1, 0, "right"));
-                    }
+                if (message.startsWith("UPDATE")) {
+                    String[] parts = message.split(" ");
+                    String name = parts[1];
+                    int x = Integer.parseInt(parts[2]);
+                    int y = Integer.parseInt(parts[3]);
+                    String direction = parts[4];
+                    int points = Integer.parseInt(parts[5]);
+
+                    // Push to JavaFX thread
+                    Platform.runLater(() -> gui.updatePlayer(name, x, y, direction, points));
                 }
             }
-        } catch (
-                Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
