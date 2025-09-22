15e4759ba5de8d59a14ecf9c7af52569f15a6f86
KarstenK <karsten-kirkegaard@hotmail.com>
2025-09-22 21:18:13 +0200

    update til branch

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -10,13 +10,17 @@ public class TCPClient {
     private Socket socket;
     private DataOutputStream out;
     private BufferedReader in;
-    private GUI gui;  // reference to GUI
+    private GUI gui;
+    private String playerName;
 
-    public TCPClient(String host, int port, GUI gui) throws IOException {
+    public TCPClient(String host, int port, GUI gui,String playerName) throws IOException {
         this.gui = gui;
+        this.playerName = playerName;
         socket = new Socket(host, port);
         out = new DataOutputStream(socket.getOutputStream());
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        sendMessage(playerName);
 
         // Start dedicated receive thread
         new RecieveThread(socket, gui).start();
